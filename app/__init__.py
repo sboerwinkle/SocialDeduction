7c3ed219f6ede20746f5ffb3085e2f9ff5f51531
Angelo Yang <angelo.r.yang@gmail.com>
2021-01-05 01:11:28 -0600

    currently got the room join/leave functionality working

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO()
 
+static_folder = "../client/build"
+template_folder = "../client/build"
 
 def create_app(debug=False):
     """Create an application."""
+    # app = Flask(__name__, static_url_path='',
+    #               static_folder=static_folder,
+    #               template_folder=template_folder)
+
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'TODO'
